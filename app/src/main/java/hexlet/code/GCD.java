ae892946ee8e6fd1fab0db993e574808d01b5a70
Ilya Anufriev <anufriev.ilya2016@yandex.ru>
2022-09-04 11:35:35 +0300

    add Progression game

---
+++ app/src/main/java/hexlet/code/GCD.java
@@ -0,0 +1,32 @@
+package hexlet.code;
+
+public final class GCD implements Game {
+    private static final String CONDITION = "Find the greatest common divisor of given numbers.";
+    private static final byte UPPERBOUND = 12;
+    private static final byte LOWERBOUND = 1;
+    public String run() {
+        int firstNumber = Engine.generateRandomNumber(LOWERBOUND, UPPERBOUND);
+        int secondNumber = Engine.generateRandomNumber(LOWERBOUND, UPPERBOUND);
+        System.out.println("Question: " + firstNumber + " " + secondNumber);
+        int tmpResult = 0;
+        tmpResult = evklid(firstNumber, secondNumber);
+        String result = String.valueOf(tmpResult);
+        return result;
+    }
+/*
+    Алгоритм Евклида.
+    Большее число делим на меньшее.
+    Если делится без остатка, то меньшее число и есть НОД (следует выйти из цикла).
+    Если есть остаток, то большее число заменяем на остаток от деления.
+    Переходим к пункту 1.
+ */
+    private int evklid(int n1, int n2) {
+        if (n2 == 0) {
+            return n1;
+        }
+        return evklid(n2, n1 % n2);
+    }
+    public String getGameCondition() {
+        return CONDITION;
+    }
+}
---
+++ app/src/main/java/hexlet/code/Progression.java
@@ -0,0 +1,42 @@
+package hexlet.code;
+
+import java.util.Arrays;
+
+public final class Progression implements Game {
+    private static final byte PROGRESSION_ELEMENTS_COUNT = 10;
+    private static final String CONDITION = "What number is missing in the progression?";
+
+    public String run() {
+        int firstNumber = Engine.generateRandomNumber();
+        int step = Engine.generateRandomNumber();
+        int randomElement =  Engine.generateRandomNumber(0, 9);
+        int[] progression = generateProgression(firstNumber, step, PROGRESSION_ELEMENTS_COUNT);
+        String question = generateQuestion(progression, randomElement);
+        System.out.println(question);
+        return String.valueOf(progression[randomElement]);
+    }
+
+    private int[] generateProgression(int firstNumber, int step, int countOfElements) {
+        int[] progression = new int[countOfElements];
+        progression[0] = firstNumber;
+        for(int i = 1; i < progression.length; i++){
+            progression[i] = progression[i - 1] + firstNumber;
+        }
+        return progression;
+    }
+
+    private String generateQuestion(int[] progression, int randomElement) {
+        String question = "Question: ";
+        for(int i = 0; i < progression.length; i++){
+            if (progression[i] == progression[randomElement]) {
+                question = question + " ..";
+            } else {
+                question = question + " " + progression[i];
+            }
+        }
+        return question;
+    }
+    public String getGameCondition() {
+        return CONDITION;
+    }
+}
